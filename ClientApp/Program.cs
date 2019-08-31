85e394418550801bdcfe5f7405d092b8eba9ae00
RavianXReaver <raveenshenal@gmail.com>
2019-08-31 16:54:21 +0530

    Add project files.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                var name = ConfigurationManager.AppSettings.Get("Name");
+
+                int port = 9999;
+                TcpClient client = new TcpClient();
+                client.Connect("localhost", port);
+                Console.WriteLine("Connected to the server!");
+                NetworkStream ns = client.GetStream();
+                Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+
+                thread.Start(client);
+
+                StringBuilder sb = new StringBuilder();
+                string s;
+
+                while (!string.IsNullOrEmpty((s = Console.ReadLine())))
+                {
+                    DeletePrevConsoleLine();
+                    sb.Append($"{name}: {s}");
+                    byte[] buffer = Encoding.ASCII.GetBytes(sb.ToString());
+                    ns.Write(buffer, 0, buffer.Length);
+                    sb.Clear();
+                }
+
+                client.Client.Shutdown(SocketShutdown.Send);
+                thread.Join();
+
+                ns.Close();
+                client.Close();
+
+                Console.WriteLine("disconnected from server!!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+
+        static void ReceiveData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] receivedBytes = new byte[1024];
+            int byte_count;
+
+            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            {
+                Console.Write(Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
+            }
+        }
+
+        private static void DeletePrevConsoleLine()
+        {
+            if (Console.CursorTop == 0) return;
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    class Program
+    {
+        static readonly object _lock = new object();
+        static readonly Dictionary<int, TcpClient> list_clients = new Dictionary<int, TcpClient>();
+
+        static void Main(string[] args)
+        {
+            int count = 1;
+
+            TcpListener ServerSocket = new TcpListener(IPAddress.Any, 9999);
+            ServerSocket.Start();
+
+            try
+            {
+                while (true)
+                {
+                    TcpClient client = ServerSocket.AcceptTcpClient();
+                    lock (_lock) list_clients.Add(count, client);
+                    Console.WriteLine($"{client.Client.RemoteEndPoint} Connected!");
+                    Broadcast($"\t{client.Client.RemoteEndPoint} Welcome To the Hell!");
+                    Thread t = new Thread(Handle_clients);
+                    t.Start(count);
+                    count++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Handle_clients(object o)
+        {
+            int id = (int)o;
+            TcpClient client;
+
+            lock (_lock) client = list_clients[id];
+
+            try
+            {
+                while (true)
+                {
+                    NetworkStream stream = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int byte_count = stream.Read(buffer, 0, buffer.Length);
+
+                    if (byte_count == 0)
+                    {
+                        break;
+                    }
+
+                    string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                    Broadcast(data);
+                    Console.WriteLine(data);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{client.Client.RemoteEndPoint} Has Escaped The Hell!");
+            }
+
+            lock (_lock) list_clients.Remove(id);
+            client.Client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+        public static void Broadcast(string data)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(data + Environment.NewLine);
+
+            lock (_lock)
+            {
+                foreach (TcpClient c in list_clients.Values)
+                {
+                    NetworkStream stream = c.GetStream();
+
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
